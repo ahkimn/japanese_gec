2ccf4894682ab6a928b185d1d6e4b03323654879
ahkimn <akimn@mit.edu>
2019-09-25 00:52:00 +0900

    clean up gitignore

--- main.py
+++ main.py
@@ -1,68 +1,83 @@
+import ast
 import sys
 import src.process as process
 import src.evaluate as evaluate
 import src.convert as convert
+import src.embedding as embedding
+import src.load as load
+
 
 def process_csv(input_file, output_source, output_target):
 
-	process.pre_process_csv(input_file, output_source, output_target)
+    process.pre_process_csv(input_file, output_source, output_target)
+
 
 def remove_pairs(input_source, input_target, output_source, output_target, same):
 
-	print(same)
+    process.remove_pairs(input_source, input_target,
+                         output_source, output_target, bool(int(same)))
 
-	process.remove_pairs(input_source, input_target, output_source, output_target, bool(int(same)))
 
 def sort_sentences(input_file, output_file):
 
-	process.sort_sentences(input_file, output_file)
+    process.sort_sentences(input_file, output_file)
+
 
 def eval_f(reference_file, system_file, top_k, alpha):
 
-	ret = evaluate.eval_f(reference_file, system_file, int(top_k), float(alpha))
-	print(ret)
+    ret = evaluate.eval_f(reference_file, system_file,
+                          int(top_k), float(alpha))
 
-def eval_binary(reference_file, system_file, top_k):
 
-	ret = evaluate.eval_binary(reference_file, system_file, int(top_k))
-	print(ret)
+def eval_binary(**kwargs):
+
+    evaluate.eval_binary(**kwargs)
+
 
 def find_semantic_pairs():
 
-	convert.find_semantic_pairs()
+    convert.find_semantic_pairs()
+
+
+def construct_fasttext_embedding():
+
+    embedding.default()
+
 
 def main():
 
-	# n_files = -1
-	# w2v.construct_default_model(n_files=n_files)
+    # n_files = -1
+    # w2v.construct_default_model(n_files=n_files)
 
-	# w2v.interactive()
-	# languages.compile_default_languages(n_files=n_files)
-	# database.construct_default_database(n_files=n_files)
-	# database.clean_default_database(max_length = 30)
+    # w2v.interactive()
+    # languages.compile_default_languages(n_files=n_files)
+    # database.construct_default_database(n_files=n_files)
+    # database.clean_default_database(max_length = 30)
 
-	# convert.convert_csv_rules(n_max=10000, n_search=1000000)
+    # convert.convert_csv_rules(n_max=10000, n_search=2500000)
 
-	# languages.load_default_languages()
-	# load.save_dataset()
+    load.save_dataset()
 
-	pass
+    pass
+
 
 if __name__ == '__main__':
 
-   # Accept input in form of (function_name, param1, param2, ..., paramX)
-   params = []
+    # Accept input in form of (function_name, param1, param2, ..., paramX)
+    params = []
 
-   if len(sys.argv) > 2:
+    argv = sys.argv
 
-      for x in range(2, len(sys.argv)):
+    kwargs = {}
+    method, args = argv[1], argv[2:]
 
-         params.append(sys.argv[x])
+    for arg in args:
 
-   if len(params) > 0:
+        kwarg, val = arg.split('=')
 
-      globals()[sys.argv[1]](*params)
+        try:
+        	val = ast.literal_eval(val)
 
-   else:
+        kwargs[kwarg] = val
 
-      globals()[sys.argv[1]]()
+    globals()[method](**kwargs)
